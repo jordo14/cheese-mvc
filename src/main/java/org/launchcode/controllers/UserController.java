dbddbc9e3ce8a5f6df3ffb811e6fdf13c7335423
Jordan Wills <jordan.millikin14@gmail.com>
2017-06-05 17:57:00 -0500

    moving stuff

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,7 @@
+package org.launchcode.controllers;
+
+/**
+ * Created by jordanwills on 6/5/17.
+ */
+public class UserController {
+}
---
+++ src/main/java/org/launchcode/models/User.java
@@ -0,0 +1,7 @@
+package org.launchcode.models;
+
+/**
+ * Created by jordanwills on 6/5/17.
+ */
+public class User {
+}
