aa86ac1cdcd5411ed3b10ce1bf4101792afb634f
Jordan Wills <jordan.millikin14@gmail.com>
2017-06-05 18:16:41 -0500

    added some edit code

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -6,10 +6,7 @@ import org.launchcode.models.CheeseType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -68,5 +65,20 @@ public class CheeseController {
 
         return "redirect:";
     }
+
+//    @RequestMapping(value = "edit/{cheeseId}", method = RequestMethod.GET)
+//    public String displayEditForm(Model model, @PathVariable int cheeseId) {
+//        Cheese c = CheeseData.getById(cheeseId);
+//        model.addAttribute("cheese", c);
+//        return "cheese/edit";
+//    }
+//
+//    @RequestMapping(value = "edit", method = RequestMethod.POST)
+//    public String processEditForm(int cheeseId, String name, String description) {
+//        Cheese c = CheeseData.getById(cheeseId);
+//        c.setName(name);
+//        c.setDescription(description);
+//        return "redirect:";
+//    }
 
 }
