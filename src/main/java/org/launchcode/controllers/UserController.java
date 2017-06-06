5149373fe514163d463e0121dcfa55533754b21d
Jordan Wills <jordan.millikin14@gmail.com>
2017-06-05 19:01:24 -0500

    only need last part of studio

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,7 +1,32 @@
 package org.launchcode.controllers;
 
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by jordanwills on 6/5/17.
  */
+
+@Controller
+@RequestMapping("user")
 public class UserController {
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+
+        return "user/add";
+
+    }
+
+//    @RequestMapping(value = "add", method = RequestMethod.POST)
+//    public String add(Model model, User user, String verify) {
+//
+//        if( ) {
+//
+//        }
+//    }
 }
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -3,5 +3,50 @@ package org.launchcode.models;
 /**
  * Created by jordanwills on 6/5/17.
  */
+
+
 public class User {
+
+    private String username;
+    private String password;
+    private String email;
+
+//    private int userId;
+//    private static int nextUserId = 1;
+
+    public User(String username, String password, String email) {
+        this();
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User() {
+//        userId = nextUserId;
+//        nextUserId++;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
