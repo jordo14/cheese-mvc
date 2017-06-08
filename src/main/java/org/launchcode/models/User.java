e48cd501e73c5d9a8b28d4ff896ccd924793c9ae
Jordan Wills <jordan.millikin14@gmail.com>
2017-06-08 18:45:50 -0500

    validation

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -6,20 +6,23 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 /**
- * Created by LaunchCode
+ * Created by Jordan Wills
  */
 public class User {
 
     @NotNull
-    @Size(min = 5, max = 15)
+    @Size(min= 5, max= 15)
     private String username;
 
     @Email(message = "Invalid email address")
     private String email;
 
     @NotNull
-    @Size(min = 5, message = "Password must be at least 5 characters long")
+    @Size(min=5, message = "Password must be at least 5 characters long")
     private String password;
+
+    @NotNull(message = "Passwords do not match")
+    private String verifyPassword;
 
     public User(String username, String email, String password) {
         this.username = username;
@@ -27,8 +30,7 @@ public class User {
         this.password = password;
     }
 
-    public User() {
-    }
+    public User() {}
 
     public String getUsername() {
         return username;
@@ -52,5 +54,22 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword() {
+        if (password != null && verifyPassword != null
+                && !password.equals(verifyPassword)) {
+            verifyPassword = null;
+        }
     }
 }
